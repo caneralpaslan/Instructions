8a78a83c0a5bf51c27391e4cc24774e808ca22d2
Caner Alpaslan <caneralpaslan@hotmail.com>
2023-08-31 17:55:39 +0300

    Remove example

--- Examples/Example/Snapshot Tests/Extensions/UIDeviceOrientation+Convenience.swift
+++
@@ -1,9 +0,0 @@
-// Copyright (c)  2021-present Frédéric Maquin <fred@ephread.com> and contributors.
-// Licensed under the terms of the MIT License.
-
-import UIKit
-
-extension UIDeviceOrientation {
-    var isLandscape: Bool { [.landscapeLeft, .landscapeRight].contains(self) }
-    var isPortrait: Bool { [.portrait, .portraitUpsideDown, .unknown].contains(self) }
-}
